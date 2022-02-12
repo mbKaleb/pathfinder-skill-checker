250ca64700f026965b895964e7d4545dc94acf8e
Kaleb <kaleb.franken7@gmail.com>
2022-02-12 14:33:39 -0700

    First Commit

---
+++ main.js
@@ -0,0 +1,47 @@
+/*
+
+
+function fetchEXAMPLE(){
+    const response = await fetch(`https://api.pathfinder2.fr/v1/pf2/`{
+
+    })
+
+}
+
+
+async function postData(url = '', data = {}) {
+  // Default options are marked with *
+  const response = await fetch(url, {
+    method: 'GET',
+    headers: {
+        'Authorization': 'da468b89-2bf8-4e2b-a939-79c6e6ef25ce',
+    },
+     
+  });
+  return response.json(); // parses JSON response into native JavaScript objects
+}
+
+
+
+
+
+
+
+
+
+
+
+
+function showCharacter() {
+    fetch(`https://api.pathfinder2.fr/v1/pf2/`, {
+        method:'GET',
+        headers: {
+            'Authorization': 'da468b89-2bf8-4e2b-a939-79c6e6ef25ce',
+            
+        }
+    }).then()
+    
+  }
+
+  
+  */
---
+++ notes.md
@@ -0,0 +1,8 @@
+MVP Deliverables
+[] - See a clickable button to create a new sheet
+[] - This sheet must be editable and contain all the relevant data
+[] - Roll a dice for each stat
+[] - check each roll against each 
+[] - 
+[]
+[]
