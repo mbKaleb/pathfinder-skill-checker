9922eae7d7e4c90801d08d7365f52039a2caada5
Kaleb <kaleb.franken7@gmail.com>
2022-02-12 20:02:43 -0700

    New Character button working, with relevant properties

--- main.js
+++ main.js
@@ -1,58 +1,63 @@
-
-
-const BaseURL = "https://api.pathfinder2.fr/v1/pf2/";
-
-fetch(BaseURL, {
-  method: "GET",
-  withCredentials: true,
-  headers: {
-    "Authorization": "da468b89-2bf8-4e2b-a939-79c6e6ef25ce",
-    "Content-Type": "application/json"
-  }
-})
-  .then(resp => resp.json())
-  .then(function(data) {
-    console.log(data);
-  })
-  .catch(function(error) {
-    console.log(error);
-  });
+// Helper Grabbers
+toolBox = document.querySelector("#tool-box")
+sheetContainer = document.querySelector("#sheet-container")
 
 
 
+//renders
+
+    const skills = ['acrobatics' ,'arcana', 'athletics', 'crafting', 'deception', 'diplomacy', 'intimidation', 'medicine', 'Nature', 'Occultism', 'performance', 'religion', 'society', 'stealth', 'survival', 'thievery', 'farming'];
 
 
-  /*
-  getData('https://api.pathfinder2.fr/v1/pf2');
 
+  function makeEmptySheet(){//make empy character sheet in the sheet container
 
-function getData(url) {
-  
-  const response = await fetch(url, {
-    method: 'GET',
-    withCredentials: true,
-    headers: {
-        'Authorization': 'da468b89-2bf8-4e2b-a939-79c6e6ef25ce',
-        'Content-Type' : 'application/json',
-    },
-     
-  })
-  .then(resp => resp.json())
-  .then(function(data) {
-    console.log(data)
-  })
+    let charsheet = document.createElement('div')
+        charsheet.className = 'character-sheet'
+
+    let level = document.createElement('h3')
+    
+    let perception = document.createElement('h3')
+    
+
+    const skillList = document.createElement('ul')
+        skillList.className = 'skill-list'
+
+      skills.forEach((skill) => {
+        listObject = document.createElement('li')
+        skillList.className = 'list-child'
+        listObject.class = skill
+        skillList.appendChild(listObject)
+
+      })
+
+  charsheet.append(level, perception, skillList)
+  sheetContainer.appendChild(charsheet)
 }
 
 
 
+//forms
+function charMake(){}
 
-function fetchApi() {
-    fetch(`https://api.pathfinder2.fr/v1/pf2/`, {
-        method:'GET',
-        headers: {
-            'Authorization': 'da468b89-2bf8-4e2b-a939-79c6e6ef25ce',
-        }
-    }).then()
+
+
+//buttons
+
+  function createBtn(){
+  
+    const bttn = document.createElement("button");
+    bttn.className = 'like-bttn';
+    bttn.textContent = 'NEW CHARACTER'
+    bttn.addEventListener('click', makeEmptySheet)
+      toolBox.appendChild(bttn, )
   }
 
-  */
+
+
+
+
+
+//Initilizers
+createBtn();
+
--- notes.md
+++ notes.md
@@ -1,3 +1,4 @@
+NO INNER HTML
 MVP Deliverables
 [] - See a clickable button to create a new sheet
 [] - This sheet must be editable and contain all the relevant data
@@ -6,3 +7,154 @@ MVP Deliverables
 [] - 
 []
 [] - da468b89-2bf8-4e2b-a939-79c6e6ef25ce
+
+[]- once thje button is clicked, create an empty sheet with referenceable variables
+
+
+Mods ( level, prof[t,e l], Temp[equipment], Item)
+
+Level
+Perception
+
+
+Acrobatics 
+Arcana
+Athletics
+Crafting
+Deception
+Diplomacy
+Intimidation
+Medicine
+Nature
+Occultism
+Performance
+Religion
+Society
+Stealth
+Survival
+Thievery
+Farming
+
+
+function Notes
+
+{
+createBtn()
+Will create a button with a paramater for an Id,
+
+This btn creates a new sheet and calls the edit form to fill data for the sheet.
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+const BaseURL = "https://api.pathfinder2.fr/v1/pf2/";
+
+fetch(BaseURL, {
+  method: "GET",
+  withCredentials: true,
+  headers: {
+    "Authorization": "da468b89-2bf8-4e2b-a939-79c6e6ef25ce",
+  }
+})
+  .then(resp => resp.json())
+  .then(function(data) {
+    console.log(data);
+  })
+  .catch(function(error) {
+    console.log(error);
+  });
+
+
+
+
+
+  
+  getData('https://api.pathfinder2.fr/v1/pf2');
+
+
+function getData(url) {
+  
+  const response = await fetch(url, {
+    method: 'GET',
+    withCredentials: true,
+    headers: {
+        'Authorization': 'da468b89-2bf8-4e2b-a939-79c6e6ef25ce',
+        'Content-Type' : 'application/json',
+    },
+     
+  })
+  .then(resp => resp.json())
+  .then(function(data) {
+    console.log(data)
+  })
+}
+
+
+
+
+function fetchApi() {
+    fetch(`https://api.pathfinder2.fr/v1/pf2/`, {
+        method:'GET',
+        headers: {
+            'Authorization': 'da468b89-2bf8-4e2b-a939-79c6e6ef25ce',
+        }
+    }).then()
+  }
+
+  */
+
+
+  mock object
+
+let characterName = {
+    name: 'yourmother'
+    level: playerlevel
+    perception: perceptionvalue
+    
+    skills: {
+      "data": "attribute"
+      "data": "attribute"
+      "data": "attribute"
+      "data": "attribute"
+      "data": "attribute"
+      "data": "attribute"
+      "data": "attribute"
+      "data": "attribute"
+      "data": "attribute"
+      "data": "attribute"
+      "data": "attribute"
+      "data": "attribute"
+      "data": "attribute"
+      "data": "attribute"
+      "data": "attribute"
+      "data": "attribute"
+  }
